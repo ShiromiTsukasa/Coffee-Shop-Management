81f51624a212716dc478546846569009715b4295
Kuroyuki-Kaze <teng.thaisothyrak@gmail.com>
2022-04-10 16:57:18 +0700

    update on mainOrderScene.fxml and controller, and some new additions to userProfile

--- src/main/java/com/UserAsClient/Controller/UserProfile.java
+++ src/main/java/com/UserAsClient/Controller/UserProfile.java
@@ -1,11 +1,48 @@
 package com.UserAsClient.Controller;
 
+
+import java.security.MessageDigest;
+
+import org.json.*;
+
 public class UserProfile {
     public String userName;
+    public JSONObject currentOrder;
+    private String userHash;
+
     UserProfile(String userName){
         this.userName = userName;
+        this.currentOrder = new JSONObject();
+
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+            byte[] messageDigest = md.digest(userName.getBytes());
+            StringBuilder sb = new StringBuilder();
+
+            for (byte b : messageDigest) {
+                sb.append(String.format("%02x", b));
+            }
+
+            this.userHash = sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
     public String getUserName(){
         return userName;
+    }
+
+    public String getUserHash(){
+        return userHash;
+    }
+
+    public JSONObject getCurrentOrder(){
+        return currentOrder;
+    }
+
+    public void setCurrentOrder(JSONObject currentOrder){
+        this.currentOrder = currentOrder;
     }
 }
