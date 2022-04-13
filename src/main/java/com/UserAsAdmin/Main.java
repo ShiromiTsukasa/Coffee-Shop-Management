34961fd7817cd2241553b09f8c62f7cfc5cc268e
Narin <hongchhiev@gmail.com>
2022-04-13 22:22:18 +0700

    i made adminAccount, 2 fxmlfile and 2 controller for admin

--- src/main/java/com/UserAsAdmin/Main.java
+++ src/main/java/com/UserAsAdmin/Main.java
@@ -1,4 +1,23 @@
 package com.UserAsAdmin;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public void start(Stage stage) throws Exception {
+       // System.out.println(com.UserAsClient.Main.class);
+        FXMLLoader fxmlLoader = new FXMLLoader(com.UserAsAdmin.Main.class.getResource("adminLogin.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 600, 400);
+        stage.setTitle("adminLogin");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
